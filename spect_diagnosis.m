49c1affe8071a1b0369c866332aa5fed0c50b4a9
irene <paspatoufi@gmail.com>
2014-10-11 16:49:44 +0300

    Calculate error

--- spect_diagnosis.m
+++ spect_diagnosis.m
@@ -1,27 +1,44 @@
-Data = importdata('spect_training.txt');
-[N,M] = size(Data); 
-r = Data(:,1);     % final diagnosis
-x = Data(:, 2:23);   % image features
+TrainData = importdata('spect_training.txt');
+TestData = importdata('spect_test.txt');
+
+[N,M] = size(TrainData); 
+r_train = TrainData(:,1);     % final diagnosis
+x_train = TrainData(:, 2:23);   % image features
+r_test = TestData(:,1);
+x_test = TestData(:, 2:23);
+
 a = 1;
 K = 2;  % number of classes
 
-idx_0 = (find(Data(:,1) == 0));
-idx_1 = (find(Data(:,1) == 1));
+idx_0 = (find(r_train == 0));
+idx_1 = (find(r_train == 1));
 
-mle_pr = sum(r)/N 
-% mle_p_red = sum(x_red(:,feat)) / length(r_red)
-% mle_p_white = sum(x_white(:,feat)) / length(r_white)
-mle_p_0 = sum(x(idx_0,:)) / length(r(idx_0))
-mle_p_1 = sum(x(idx_1,:)) / length(r(idx_1))
-% for j = 1:22
-%     mle_p_0(j) = (a + sum(r(idx_0)' * x(idx_0,j))) / (K*a + length(r(idx_0)));
-%     mle_p_1(j) = (a + sum(r(idx_1)' * x(idx_1,j))) / (K*a + sum(r(idx_1)));
-%     w_0(j) = sum( log(1-mle_p_0(j)) ) + log(1-mle_pr) - sum( log(1-mle_p_1(j)) ) - log(mle_pr);
-%     w = sum( log( mle_p_0(j)*(1-mle_p_1(j)) ) ) - sum( log( mle_p_1(j)*(1-mle_p_0(j)) ) );
-% end
-% mle_p_0, mle_p_1, w_0, w
+mle_p = ones(2,22);     % initialize p_ij MLE
+w = ones(1,22);     % initialize w parameter
 
-w_0 = sum( log(1-mle_p_0) ) + log(1-mle_pr) - sum( log(1-mle_p_1) ) - log(mle_pr)
-w = sum( log( mle_p_0.*(1-mle_p_1) ) ) - sum( log( mle_p_1.*(1-mle_p_0) ) )
+mle_pr = sum(r_train)/N ;
+for j = 1:22
+    mle_p(1,j) = (a + sum(r_train(idx_0)' * x_train(idx_0,j))) / (K*a + length(r_train(idx_0)));
+    mle_p(2,j) = (a + sum(r_train(idx_1)' * x_train(idx_1,j))) / (K*a + sum(r_train(idx_1)));     
+                  % --> since r(idx_1(i)) == 1, we assume that sum(r) == length(r) <--
+    w(j) = log( mle_p(1,j)) - log( 1-mle_p(1,j) )- log( mle_p(2,j) ) + log( 1-mle_p(2,j) );
+end
+w_0 = sum( log(1-mle_p(1,:)) ) + log(1-mle_pr) - sum( log(1-mle_p(2,:)) ) - log(mle_pr);
 
-% err_train = TO BE CONTINUED
+discr_train = 1 ./(1+exp(w*(x_train') + w_0));
+pred_train = (discr_train > mle_pr);
+err_train = sum(abs(r_train - pred_train'))/100
+
+discr_test = 1 ./(1+exp(w*(x_test') + w_0));
+pred_test = (discr_test > mle_pr);
+err_test = sum(abs(r_test - pred_test'))/100
+
+
+% fprintf('The maximum likelihood estimator for p_r is %f\n', mle_pr);
+% fprintf('The maximum likelihood estimator for p_ij is '); mle_p
+% fprintf('The parameters of the sigmoid form are w, w_0: '); w, w_0
+% fprintf('The error for the training set is %f\n ', err_train);
+% fprintf('The error for the test set is %f\n ', err_test);
+
+
+% err_train = sum( r(:,) - pred )/100;
